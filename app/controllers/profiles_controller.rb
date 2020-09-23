c14bf5f47f3cfcbe9a3dc0a43aac657c699e3eb4
masao <masao>
2020-09-23 23:12:37 +0900

    json形式でデータを送ることができるようになりました。

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,3 @@
-
 class ProfilesController < ApplicationController
   before_action :set_profile, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
@@ -7,12 +6,10 @@ class ProfilesController < ApplicationController
   def index
     @profiles = Profile.where(:is_enable => true)
   end
-
   # GET /profiles/1
   # GET /profiles/1.json
   def show
   end
-
   # GET /profiles/new
   def new
     @profile = Profile
@@ -21,20 +18,17 @@ class ProfilesController < ApplicationController
     @profile.careers.build
     redirect_to edit_profile_url(@profile)
   end
-
   # GET /profiles/1/edit
   def edit
-    if  @profile.user_id != current_user.id
-      raise ActionController::RoutingError.new('Not authorized')
+    if @profile.user_id != current_user.id
+      raise ActionController::RoutingError.new("Not authorized")
     end
   end
-
   # POST /profiles
   # POST /profiles.json
   def create
     @profile = Profile.new(profile_params)
     # @profile.save!
-
     # respond_to do |format|
     #   if @profile.save
     #     format.html { redirect_to @profile, notice: 'Profile was successfully created.' }
@@ -45,24 +39,27 @@ class ProfilesController < ApplicationController
     #   end
     # end
   end
-
   # PATCH/PUT /profiles/1
   # PATCH/PUT /profiles/1.json
   def update
     respond_to do |format|
       if @profile.update(profile_params)
-        format.html { redirect_to @profile, notice: 'Profile was successfully updated.' }
+        format.html { redirect_to @profile, notice: "Profile was successfully updated." }
         format.json { render :show, status: :ok, location: @profile }
         @profiles = Profile.where(:is_enable => true)
-        profiles_json = render_to_string(template:'profiles/index.json.jbuilder')
-        ProfilesHelper.upload_to_s3(profiles_json)
+        profiles_json = render_to_string(template: "profiles/index.json.jbuilder")
+        if Rails.env == "development"
+          key = "dev-master.json"
+        else
+          key = "master.json"
+        end
+        ProfilesHelper.upload_to_s3(key, profiles_json)
       else
         format.html { render :edit }
         format.json { render json: @profile.errors, status: :unprocessable_entity }
       end
     end
   end
-
   # DELETE /profiles/1
   # DELETE /profiles/1.json
   # def destroy
@@ -72,19 +69,13 @@ class ProfilesController < ApplicationController
   #     format.json { head :no_content }
   #   end
   # end
-
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_profile
-      @profile = Profile.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def profile_params
-      params.require(:profile).permit(:name, :birthday, :address, :is_enable, :thumbnail, :header_image, :introduction, :user_id, articles_attributes:  [:profile_id, :user_id, :title, :category, :image, :text, :url, :date, :_destroy, :id ], careers_attributes:[:date, :text, :user_id, :profile_id, :_destroy, :id ]
-      )
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_profile
+    @profile = Profile.find(params[:id])
+  end
+  # Only allow a list of trusted parameters through.
+  def profile_params
+    params.require(:profile).permit(:name, :birthday, :address, :is_enable, :thumbnail, :header_image, :introduction, :user_id, articles_attributes: [:profile_id, :user_id, :title, :category, :image, :text, :url, :date, :_destroy, :id], careers_attributes: [:date, :text, :user_id, :profile_id, :_destroy, :id])
+  end
 end
-
-
